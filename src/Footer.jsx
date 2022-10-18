aa84ef5265135f117617847ab79fad065293667d
Phynnnix <phynnnix@tutanota.com>
2022-10-19 01:25:56 +0200

    Footer got a neat text

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,11 @@
 let Footer = () => {
     return (
-    <footer className="p5 bg-primary text-white">
-        
+    <footer className="container bg-primary text-white">
+        <div className="row justify-content-center mt-3 mb-4">
+            <div className="col-8">
+                <p>No Backend Team Management App - {new Date().getFullYear()}</p>
+            </div>
+        </div>
     </footer>
     )
 };
