65fe6b1208efdb8a10d4ab1710f656c561a93e39
Phynnnix <phynnnix@tutanota.com>
2022-10-19 01:26:48 +0200

    Header displays selected Team and its number of members

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,14 @@
-let Header = () => {
+import getTeamNameByTag from './teamList';
+
+let Header = ({selectedTeam, teamMemberCount}) => {
     return (
-        <header className="p5 bg-primary text-white">
-            
+        <header className="container bg-primary text-white">
+            <div className="row justify-content-center mt-3 mb-4" >
+                <div className="col-8">
+                    <h1>Team Member Allocation</h1>
+                    <h3>{getTeamNameByTag(selectedTeam)} has {teamMemberCount} {teamMemberCount === 1?"member":"members"}.</h3>
+                </div>
+            </div>
         </header>
     )
 };
